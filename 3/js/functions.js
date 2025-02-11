4df84302d74c143ce26fb5aee90ecb78747f550e
Keks <keks@htmlacademy.ru>
2025-02-11 19:01:43 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,19 @@
+//Функция проверки длины строки
+function checkStringLength(string, maxLength) {
+  return string.length <= maxLength;
+}
+window.console.log(checkStringLength('Привет, мир!', 15)); // Вывод результата в консоль
+
+//Функция проверки палиндрома
+function isPalindromeCheck(string) {
+  const cleanedStr = string.replaceAll(' ', '').toLowerCase();
+  let reversedStr = '';
+  for (let i = cleanedStr.length - 1; i >= 0; i--) {
+    reversedStr += cleanedStr[i];
+  }
+  return cleanedStr === reversedStr;
+}
+window.console.log(isPalindromeCheck('А роза упала на лапу Азора')); // Вывод результата в консоль
+window.console.log(isPalindromeCheck('ДовОд')); // Вывод результата в консоль
+window.console.log(isPalindromeCheck('Привет')); // Вывод результата в консоль
+
